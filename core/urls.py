a35c2cd27248c3e7a6eb12c653320141c896e32b
kethelynsantos <kethelyngabrielly14@gmail.com>
2023-11-11 19:58:02 -0300

    update

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+# core/urls.py
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from .views import CustomUserViewSet, ClientViewSet
@@ -8,5 +9,5 @@ router.register('client', ClientViewSet)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
-    # path('api/v1/auth/register/', ClientViewSet.as_view(), name='create_user'),
+    path('api/v1/auth/register/', ClientViewSet.as_view({'post': 'register'}), name='create_user'),
 ]
